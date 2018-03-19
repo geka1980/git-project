5b8a3ff42d602d93818c3f40944293d0e773f705
evgeniy <evgenij.kropachev@yandex.ru>
2018-03-19 12:30:13 +0700

    github

--- LESENSE.php
+++ LESENSE.php
@@ -2,4 +2,4 @@
 
     echo 'Привет мир!!!';
     echo 'Привет git!!!';
-    echo 'Привет branch master github!!!';
+    echo 'Привет branch master github2!!!';
--- htdocs/index.php
+++ htdocs/index.php
@@ -2,4 +2,4 @@
 
     echo 'Привет мир!!!';
     echo 'Привет branch master!!!';
-    echo 'Привет branch master github!!!';
+    echo 'Привет branch master github2!!!';
